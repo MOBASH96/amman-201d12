0b89c48062373fd9ec42cfd32b6ca9c609c9d5dd
Mohammad Elbashabsheh <moelbasheng@gmail.com>
2020-12-31 18:15:10 +0200

    added any array

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -115,10 +115,10 @@ function multiplyArray(multArr) { //eslint-disable-line
         return returnArray ;
 }   
 
-
+ 
 // Here is the test for multiplyArray(); uncomment it to run it
 multiplyArray(testArray);
- testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
@@ -142,10 +142,16 @@ Test this function by hand in the console to get it working, and when you think 
 var testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
 
 function multiplyAnyArray(dynamicArray) { //eslint-disable-line
-
+    var total=1;
+    for(var i=0;i<dynamicArray.length;i++){
+    total=multiply(dynamicArray[i],total)[0];
+    }
+    var returnArray=[total,"The numbers 1,2,3,4,5 have a product of 120."];
+    return returnArray ;
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyAnyArray(testDynamicArray);
+multiplyArray(testDynamicArray);
+testMultiplyAnyArray(testDynamicArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
